111a399c61a22879999b4c7c0728b8283c698172
Kirilukich <theflashan@gmail.com>
2023-01-30 03:12:42 +0300

    Task 5.2 order overview

---
+++ src/main/java/com/es/phoneshop/web/OrderOverviewPageServlet.java
@@ -0,0 +1,33 @@
+package com.es.phoneshop.web;
+
+import com.es.phoneshop.model.cart.Cart;
+import com.es.phoneshop.model.cart.CartService;
+import com.es.phoneshop.model.cart.DefaultCartService;
+import com.es.phoneshop.model.order.*;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class OrderOverviewPageServlet extends HttpServlet {
+    private OrderDao orderDao;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        orderDao = ArrayListOrderDao.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String securedOrderId = request.getPathInfo().substring(1);
+        request.setAttribute("order", orderDao.getOrderBySecureId(securedOrderId));
+        request.getRequestDispatcher("/WEB-INF/pages/orderOverview.jsp").forward(request, response);
+    }
+}
